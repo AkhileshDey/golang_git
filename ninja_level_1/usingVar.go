5c5d5aee1c7a42846bca457e0aa7739302e9f873
Akhilesh Dey <akhilesh.dey@gmail.com>
2020-01-11 15:29:55 +0530

    using Var declare variable/Zero value concept

---
+++ ninja_level_1/usingVar.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println("zero value- since no value is assigned")
+	fmt.Println("zero value for type int is 0 ")
+	fmt.Println("zero value of type string is empty string")
+	fmt.Println("zero value of type boolean is false")
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
